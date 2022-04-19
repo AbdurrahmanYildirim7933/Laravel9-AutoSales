5bfa55bcac714d9152c0bc6a8c5b81085fcb2245
AbdurrahmanYildirim7933 <nogay0601@gmail.com>
2022-04-19 15:06:05 +0300

    Admin Sidebar Links_Icons

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -10,7 +10,8 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Category List</h1>
+                        <a href="/admin/category/create" class="btn btn-info btn-fw" style="width: 200px">Add Category</a>
+
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
